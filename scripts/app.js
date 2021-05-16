3b1d517e08c126b60df25d01d0d2c60f44aad5ea
Avro <jyotirmoy.avro@gmail.com>
2021-05-16 15:07:32 +0600

    Used await, cleaned JS, added lots of quote

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,38 @@
 const QUOTEBTN = document.querySelector(".get-quote");
 const COPYBTN = document.querySelector(".copy-quote");
+const QUOTE = document.querySelector(".quote");
+const AUTHOR = document.querySelector(".author");
+
+let quotes = [];
+
+const TOAST = {
+    init() {
+        this.hideTimeout = null;
+        this.el = document.createElement("div");
+        this.className = "toast toast--visible";
+        document.body.appendChild(this.el);
+    },
+
+    show() {
+        clearTimeout(this.hideTimeout);
+        this.el.innerText = "Copied!"
+        this.el.className = "toast toast--visible"
+        this.hideTimeout = setTimeout(() => {
+            this.el.classList.remove('toast--visible');
+        }, 2800);
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => TOAST.init());
 
 function Clipboard_CopyTo(quote, author) {
-    var tempInput = document.createElement("input");
-    tempInput.value = `${quote} ${author}`;
+    let tempInput = document.createElement("input");
+    tempInput.value = `${quote} - ${author}`;
     document.body.appendChild(tempInput);
     tempInput.select();
     document.execCommand("copy");
     document.body.removeChild(tempInput);
+    TOAST.show();
 }
 
 COPYBTN.onclick = function() {
@@ -16,29 +41,16 @@ COPYBTN.onclick = function() {
 
 QUOTEBTN.addEventListener("click", getQuote);
 
-function getQuote() {
-    fetch('scripts/quotes.json')
-        .then(response => response.json())
-        .then(data => {
-            let randomNum = randomNumber();
-            let quote = data[randomNum].quote;
-            let author = data[randomNum].author;
-            document.querySelector(".quote").innerText = quote;
-            document.querySelector(".author").innerText = `- ${author}`;
-            COPYBTN.addEventListener("click", Clipboard_CopyTo(document.querySelector(".quote").innerText, document.querySelector(".author").innerText));
-
-            function Clipboard_CopyTo(quote, author) {
-                var tempInput = document.createElement("input");
-                tempInput.value = `${quote} - ${author}`;
-                document.body.appendChild(tempInput);
-                tempInput.select();
-                document.execCommand("copy");
-                document.body.removeChild(tempInput);
-            }
-
-        });
+async function getQuote() {
+    response = await fetch("scripts/quotes.json");
+    quotes = await response.json();
+    randomNum = randomNumber();
+    QUOTE.innerText = quotes[randomNum].quote;
+    AUTHOR.innerText = quotes[randomNum].author;
 }
 
 function randomNumber() {
     return Math.floor(Math.random() * 102);
 }
+
+getQuote();
